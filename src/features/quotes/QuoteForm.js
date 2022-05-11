a220f19107c1b0869305ed5ef1d6508aafbf5826
Iuliia El Sammak <juliag875@gmail.com>
2022-05-10 22:38:58 -0400

    commit tree 55b0603cd4d31523f3eb7284d89e7ec0a2a018b4

--- src/features/quotes/QuoteForm.js
+++ src/features/quotes/QuoteForm.js
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { v4 as uuid } from "uuid";
 import { addQuote } from "./quotesSlice";
 
 function QuoteForm() {
   const [formData, setFormData] = useState({
+    content: "",
+    author: "",
     // set up a controlled form with internal state
     // look at the form to determine what keys need to go here
   });
 
+  const dispatch = useDispatch();
+
   function handleChange(event) {
-    // Handle Updating Component State
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
   }
 
+  // Handle Updating Component State
   function handleSubmit(event) {
+    event.preventDefault();
+
+    dispatch(
+      addQuote({
+        ...formData,
+        id: uuid(),
+        votes: 0,
+      })
+    );
+    setFormData({
+      content: "",
+      author: "",
+    })
     // Handle Form Submit event default
     // Create quote object from state
     // Pass quote object to action creator
@@ -25,7 +47,7 @@ function QuoteForm() {
         <div className="col-md-8 col-md-offset-2">
           <div className="panel panel-default">
             <div className="panel-body">
-              <form className="form-horizontal">
+              <form className="form-horizontal" onSubmit={handleSubmit}>
                 <div className="form-group">
                   <label htmlFor="content" className="col-md-4 control-label">
                     Quote
@@ -33,8 +55,10 @@ function QuoteForm() {
                   <div className="col-md-5">
                     <textarea
                       className="form-control"
+                      name="content"
                       id="content"
                       value={formData.content}
+                      onChange = {handleChange}
                     />
                   </div>
                 </div>
@@ -45,9 +69,11 @@ function QuoteForm() {
                   <div className="col-md-5">
                     <input
                       className="form-control"
+                      name="author"
                       type="text"
                       id="author"
                       value={formData.author}
+                      onChange = {handleChange}
                     />
                   </div>
                 </div>
--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,66 @@
+
 // Action Creators
-// TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  return {
+    type: "quotes/add",
+    payload: quote,
+  }
+}
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: "quotes/remove",
+    payload: quoteId,
+  }
+}
+
+export function upvoteQuote(quoteId) {
+  return {
+    type: "quotes/upvote",
+    payload: quoteId,
+  }
+}
+
+export function downvoteQuote(quoteId) {
+  return {
+    type: "quotes/downvote",
+    payload: quoteId,
+  }
+}
 
 // Reducer
 const initialState = [];
 
-export default function quotesReducer(state = initialState, action) {
+export default function quotesReducer(state = initialState, action) { 
+  // const { type, payload } = action; destructure action
+  switch (action.type) {
+    case "quotes/add":
+      return [...state, action.payload];
+
+    case "quotes/remove":
+      return state.filter( q => q.id !== action.payload)
+
+    case "quotes/upvote":
+      return state.map(quote => {
+        if (quote.id === action.payload) {
+          return {...quote, votes: quote.votes + 1}
+        } else {
+          return quote
+        }
+      })
+
+      case "quotes/downvote":
+      return state.map(quote => {
+        if (quote.id === action.payload && quote.votes > 0) {
+          return {...quote, votes: quote.votes - 1}
+        } else {
+          return quote
+        }
+      })
+      
+
+  default:
+    break;
+  }
   return state;
 }
